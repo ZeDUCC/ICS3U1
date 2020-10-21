0f00f95fcbd6ff9eff28318d7834332f7381a2df
Janahan <janahansiva6@gmail.com>
2020-10-20 21:51:24 -0400

    e

--- PiApproximation.java
+++ PiApproximation.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 public class PiApproximation {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to the Pi Approximation program. This program will\napproximate the value of pi given an integer n.");
-        int n = s.nextInt();
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            double piValue = (Math.pow(-1, i)) / (2*i + 1);
-            sum += piValue;
+        boolean status = true;
+        loop:
+        while (status) {
+            System.out.println("Welcome to the Pi Approximation program. This program will\napproximate the value of pi given an integer n.\n");
+            System.out.println("Enter a value.");
+            int n = s.nextInt();
+            double sum = 0;
+            for (int i = 0; i < n; i++) {
+                double piValue = (Math.pow(-1, i)) / (2*i + 1);
+                sum += piValue;
+            }
+            System.out.println("The value of pi is " + 4 * sum);
+            System.out.println("Want to try again? Type '1' for yes or '2' for no.");
+            if (s.nextInt() != 1) {
+                System.out.println("The program is now terminated.");
+                break loop;
+            }
         }
-        System.out.println("The value of pi is " + 4 * sum);
         s.close();
     }
 }
+
