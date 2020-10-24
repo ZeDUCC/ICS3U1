9402d55f55aee1e3d3250c8dd5602d3e2bd31203
Janahan <janahansiva6@gmail.com>
2020-10-24 14:45:46 -0400

    Enter

--- PolynomialEvaluator.java
+++ PolynomialEvaluator.java
@@ -4,5 +4,27 @@ public class PolynomialEvaluator {
     
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the function, f(x), that you would like to use.");
+        String function = s.nextLine();
+        System.out.println("Now, enter the value of x.");
+        double x = s.nextDouble();
+        double sum = 0;
+        String[] terms = function.split(" ");
+        for (String term : terms) {
+            if (term.contains("x") && term.contains("^")) {
+                double coefficient = Double.parseDouble(term.substring(0, term.indexOf("x")));
+                int exponent = Integer.parseInt(term.substring(term.indexOf("^") + 1));
+                sum += Math.pow(x, exponent) * coefficient;
+            } else if (!term.contains("x") && term.contains("^")){
+                sum += Math.pow(x, Integer.parseInt(term.substring(term.indexOf("^") + 1)));
+            } else if (term.contains("x") && !term.contains("^")) {
+                sum += x * Double.parseDouble(term.substring(0, term.indexOf("x")));
+            } else {
+                sum += Integer.parseInt(term);
+            }
+        } 
+        System.out.println("f(" + x + ") is " + sum);
+        s.close();
     }
 }
+
