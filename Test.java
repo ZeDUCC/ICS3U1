6597a60af6f1b61ad7b6a31c0fd6b0a0dd9c89d2
Janahan <janahansiva6@gmail.com>
2020-10-29 12:29:01 -0400

    Enter

--- Test.java
+++ Test.java
@@ -2,16 +2,19 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        String derivative1 = "+25.0x^4 +9.0x^2 +4.0x +1.0";
+        String[] coefficients = {"5", "3", "2", "1"};
+        String[] exponents = {"5", "3", "2", "1"};
         String derivative2 = "";
-        String[] d2Terms = derivative1.split(" ");
-        for (String term : d2Terms) {
-            double coValue = Double.parseDouble(term.substring(0, term.indexOf("x") - 1));
-            int expValue = Integer.parseInt(term.substring(term.indexOf("^") + 1));
-            if (coValue >= 0) {
+        loop:
+        for (int i = 0; i < coefficients.length; i++) {
+            double coValue = Double.parseDouble(coefficients[i]) * Integer.parseInt(exponents[i]);
+            int expValue = Integer.parseInt(exponents[i]) - 1;
+            if (coValue > 0) {
                 derivative2 += "+";
-            } else {
+            } else if (coValue < 0){
                 derivative2 += "-";
+            } else {
+                derivative2 += "";
             }
             if (expValue - 1 == 1 && coValue == 1) {
                 derivative2 += "+ x ";
@@ -19,13 +22,15 @@ public class Test {
                 derivative2 += coValue;
             } else if (expValue - 1 == 1) {
                 derivative2 += coValue * expValue + "x ";
-            } else if (coValue == 1 && expValue - 1 != 0) {
-                derivative2 += "x^" + (expValue - 1);
+            } else if (coValue == 1 && expValue - 1 < 0) {
+                derivative2 += "";
+            } else if (expValue < 0) {
+                continue loop;
             } else {
                 derivative2 += (coValue * expValue) + "x^" + (expValue - 1) + " ";
             }
         }
-        System.out.println(derivative2);
+        System.out.println(derivative2.substring(1));
     }
 }
 
