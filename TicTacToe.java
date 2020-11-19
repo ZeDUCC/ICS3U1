1a21d1a6673292dcb02d499ebff2b0581aee00b2
Janahan <janahansiva6@gmail.com>
2020-11-19 18:08:07 -0500

    Enter

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JanahanSivaneswaran {
+public class TicTacToe {
         
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
